60b954199866b75ade957b9977d45c6553ca91d5
Jonathan Stoppani <jonathan@stoppani.name>
2015-01-05 09:19:09 +0100

    Version bump.

--- lancet/__init__.py
+++ lancet/__init__.py
@@ -8,5 +8,5 @@ From http://en.wikipedia.org/wiki/Scalpel:
 
 __author__ = 'Jonathan Stoppani'
 __email__ = 'jonathan@stoppani.name'
-__version__ = '0.4'
+__version__ = '0.4.1'
 __url__ = 'https://github.com/GaretJax/lancet'
